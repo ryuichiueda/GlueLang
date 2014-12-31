bc39b95699dc386f391b613cb78ec745abe0ef64
Ryoichi KATO <ryo1kato@gmail.com>
2014-12-31 13:44:41 +0900

    fix Feeder::blank() for a blank line

--- Feeder.cc
+++ Feeder.cc
@@ -108,6 +108,10 @@ bool Feeder::blank(string *ans)
 		return false;
 
 	int i = m_cur_char;
+	if( p->size() < 1 ) {
+		*ans = string("");
+		return true;
+	}
 	if( p->at(i) != ' ')
 		return false;
 
